4d0c9adaf92285ced426a7fdfd7ac5c4208690b1
thibd <thib.devineau@gmail.com>
2025-11-05 09:21:22 +0100

    Instalation de twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -14,6 +14,21 @@ class LuckyController
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
+            
+        );
+    }
+    #[Route('/lucky/letter')]
+        public function letter(): Response
+    {
+        $number = random_int(0, 23);
+
+        $alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+        $letter = $alphabet[$number];
+
+        return new Response(
+            '<html><body>Lucky letter: '.$letter.'</body></html>'
+            
         );
     }
 }
